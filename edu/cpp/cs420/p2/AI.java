5b26d8add3d36967bef8081325b1bf6cd3dcd565
Caleb Chong <czchong@cpp.edu>
2017-08-31 17:06:01 -0700

    first commit

---
+++ edu/cpp/cs420/p2/AI.java
@@ -0,0 +1,6 @@
+package edu.cpp.cs420.p2;
+
+public class AI {
+	public static final int MARK = 2;
+	
+}
---
+++ edu/cpp/cs420/p2/Main.java
@@ -0,0 +1,15 @@
+package edu.cpp.cs420.p2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		State s = new State();
+		s = s.mark("A1", 1);
+		s = s.mark("B1", 1);
+		s = s.mark("C1", 1);
+		s = s.mark("D1", 1);
+		System.out.println(s);
+		System.out.println(s.terminalTest());
+	}
+
+}
